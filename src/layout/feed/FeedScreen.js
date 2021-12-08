1ba241db407d31291cc1ff03d24bdb0d59da2eeb
Snow <neoprince1992@gmail.com>
2021-12-08 06:58:31 -0800

    make feed demo page

--- src/layout/feed/FeedScreen.js
+++ src/layout/feed/FeedScreen.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, FlatList } from 'react-native';
 import { Button, TopNavigation, Text } from '@ui-kitten/components';
 import { connect } from 'react-redux';
+import FeatherIcon from 'react-native-vector-icons/dist/Feather';
+import data from './data.json';
 
 class FeedScreen extends PureComponent {
     constructor(props) {
@@ -21,22 +23,50 @@ class FeedScreen extends PureComponent {
     }
 
     renderTitle = () => {
-        return <View style={styles.header}>
+        return (
             <Text numberOfLines={1} style={{ fontWeight: 'bold', fontSize: 16 }}>
                 Activities
             </Text>
-        </View>
+        )
+    }
+
+    renderFeedItem = ({ item }) => {
+        return (
+            <View style={styles.feedItemContainer}>
+                <FeatherIcon
+                    size={16}
+                    color='#ddd'
+                    name='user'
+                />
+                <Text style={styles.feedItemUser}>
+                    {item.user}
+                    {item.type == 'like' && <Text style={styles.feedItemAction}> liked your message</Text>}
+                    {item.type == 'reply' && <Text style={styles.feedItemAction}> replied your message: {item.text}</Text>}
+                </Text>
+            </View>
+        )
     }
 
     render() {
         return (
             <View style={styles.container} >
-                <TopNavigation
-                    title={this.renderTitle}
-                    accessoryRight={this.addScoreCardActionButton}
-                />
-                <View>
+                <TopNavigation title={this.renderTitle} />
+                <View style={styles.feedContainer}>
+                    <Text style={styles.feedHeader}>This Week</Text>
+                    <FlatList
+                        data={data}
+                        style={styles.feedList}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={this.renderFeedItem}
+                    />
 
+                    <Text style={styles.feedHeader}>This Month</Text>
+                    <FlatList
+                        data={data}
+                        style={styles.feedList}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={this.renderFeedItem}
+                    />
                 </View>
             </View>
         )
@@ -52,15 +82,31 @@ export default connect(mapStateToProps, null)(FeedScreen);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: '#111',
     },
-    header: {
+    feedContainer: {
+        padding: 10,
+    },
+    feedHeader: {
+        fontSize: 14,
+        fontWeight: 'bold',
         color: 'white',
-        borderRadius: 0,
-        borderColor: 0,
-        backgroundColor: 'black',
-        fontSize: 20,
-        maxWidth: '70%',
-        alignSelf: 'center',
+        marginTop: 10
     },
+    feedList: {
+        marginTop: 5
+    },
+    feedItemContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: 4
+    },
+    feedItemUser: {
+        fontSize: 13,
+        marginLeft: 5,
+        fontWeight: 'bold'
+    },
+    feedItemAction: {
+        fontSize: 13,
+    }
 });
