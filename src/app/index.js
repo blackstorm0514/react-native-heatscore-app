6f43539607fd48daa6ef6177e7d6a0fe2376e8e5
Snow <neoprince1992@gmail.com>
2021-12-16 07:50:29 -0800

    make notification background

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,8 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { messaging } from 'react-native-firebase';
+import { messaging, notifications } from 'react-native-firebase';
+import PushNotification from 'react-native-push-notification';
 
 import AppLoading from './app-loading.component';
 import { appMappings, appThemes } from './app-theming';
@@ -15,6 +16,8 @@ import { Theming } from '../services/theme.service';
 import { VectorIconsPack } from './vector-icons-pack';
 import { store, persistor } from '../redux/store';
 import { ApiService } from '../services/api.service';
+import { CheckPermissionAndReturnFcmToken } from '../libs/fcm_utils';
+
 
 const defaultConfig = {
     userToken: null,
@@ -28,24 +31,54 @@ const App = ({ userToken }) => {
 
     const sendFcmToken = async () => {
         try {
-            const authStatus = await messaging().requestPermission();
-            const enabled =
-                authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-                authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-            if (enabled) {
-                await messaging().registerDeviceForRemoteMessages();
-                const token = await messaging().getToken();
-
-                await ApiService.post('/notifications/register', { token });
+            const fcmToken = await CheckPermissionAndReturnFcmToken();
+            if (fcmToken) {
+                await ApiService.post('/notifications/register', { token: fcmToken });
             }
-        } catch (err) {
-            console.log(err.response.data);
-            return;
         }
-    };
+        catch (error) {
+            console.warn('Error', error);
+        };
+    }
+
+    const foregroundNotificationsListener = () => {
+        // to process message broadcasted from onMessageReceived method
+        // const messageListener = messaging().onMessage((message) => {
+        //     console.log("onMessage", message);
+        //     PushNotification.localNotification({
+        //         message: remoteMessage.notification.body,
+        //         title: remoteMessage.notification.title,
+        //         bigPictureUrl: remoteMessage.notification.android.imageUrl,
+        //         smallIcon: remoteMessage.notification.android.imageUrl,
+        //     });
+        // })
+
+        notifications().onNotification((notification) => {
+            console.log(notification)
+            // notification.android.setChannelId(notification.data.channelId);
+            // notifications().displayNotification(notification);
+        });
+
+        // get notification data when notification is clicked when app is in foreground
+        const notificationOpenedListener = notifications().onNotificationOpened((notificationData) => {
+            console.log("onNotificationOpened", notificationData);
+        });
+
+        // get notification data when notification is clicked to open app when app is in background
+        notifications().getInitialNotification()
+            .then((notificationData) => {
+                console.log("getInitialNotification", notificationData);
+            });
+
+        return () => {
+            messageListener();
+            notificationOpenedListener();
+        }
+    }
 
     useEffect(() => {
         sendFcmToken();
+        return foregroundNotificationsListener();
     }, []);
 
     return (
