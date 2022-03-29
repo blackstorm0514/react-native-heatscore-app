282689fa1379da77d4896b9292eaf30c9cba0ec2
Snow <neoprince1992@gmail.com>
2022-03-29 10:34:13 +0900

    Change label positions in SelectTypeComponent

--- src/layout/scorecard/components/SelectTypeComponent.js
+++ src/layout/scorecard/components/SelectTypeComponent.js
@@ -13,37 +13,42 @@ export default class SelectTypeComponent extends PureComponent {
                     <Text style={styles.titleText}>TYPE</Text>
                 </View>
                 <View style={styles.typeContainer}>
-                    <TouchableOpacity style={styles.radioContainer}
-                        activeOpacity={0.7}
-                        onPress={() => onSelect('moneyline')}>
-                        <FontAwesomeIcon color='white' size={14}
-                            name={type == 'moneyline' ? 'check-circle' : 'circle-thin'} />
+                    <View style={styles.subContainer}>
+                        <Text style={styles.titleText}>Basic</Text>
 
-                        <Text style={styles.selectItemType} numberOfLines={1}>Moneyline</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.radioContainer}
-                        activeOpacity={0.7}
-                        onPress={() => onSelect('spread')}>
-                        <FontAwesomeIcon color='white' size={14}
-                            name={type == 'spread' ? 'check-circle' : 'circle-thin'} />
-                        <Text style={styles.selectItemType} numberOfLines={1}>Spread</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.radioContainer}
-                        activeOpacity={0.7}
-                        onPress={() => onSelect('total')}>
-                        <FontAwesomeIcon color='white' size={14}
-                            name={type == 'total' ? 'check-circle' : 'circle-thin'} />
-                        <Text style={styles.selectItemType} numberOfLines={1}>Total Over / Under</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.typeContainer}>
-                    <TouchableOpacity style={styles.radioContainer}
-                        activeOpacity={0.7}
-                        onPress={() => onSelect('total_home')}>
-                        <FontAwesomeIcon color='white' size={14}
-                            name={['total_home', 'total_away'].includes(type) ? 'check-circle' : 'circle-thin'} />
-                        <Text style={styles.selectItemType} numberOfLines={1}>Team Over / Under</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity style={styles.radioContainer}
+                            activeOpacity={0.7}
+                            onPress={() => onSelect('moneyline')}>
+                            <FontAwesomeIcon color='white' size={14}
+                                name={type == 'moneyline' ? 'check-circle' : 'circle-thin'} />
+
+                            <Text style={styles.selectItemType} numberOfLines={1}>Moneyline</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.radioContainer}
+                            activeOpacity={0.7}
+                            onPress={() => onSelect('spread')}>
+                            <FontAwesomeIcon color='white' size={14}
+                                name={type == 'spread' ? 'check-circle' : 'circle-thin'} />
+                            <Text style={styles.selectItemType} numberOfLines={1}>Spread</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.subContainer}>
+                        <Text style={styles.titleText}>Over/Under</Text>
+                        <TouchableOpacity style={styles.radioContainer}
+                            activeOpacity={0.7}
+                            onPress={() => onSelect('total')}>
+                            <FontAwesomeIcon color='white' size={14}
+                                name={type == 'total' ? 'check-circle' : 'circle-thin'} />
+                            <Text style={styles.selectItemType} numberOfLines={1}>Total</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.radioContainer}
+                            activeOpacity={0.7}
+                            onPress={() => onSelect('total_home')}>
+                            <FontAwesomeIcon color='white' size={14}
+                                name={['total_home', 'total_away'].includes(type) ? 'check-circle' : 'circle-thin'} />
+                            <Text style={styles.selectItemType} numberOfLines={1}>Team</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         )
@@ -66,7 +71,8 @@ const styles = StyleSheet.create({
     radioContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginLeft: 10,
+        marginTop: 10,
+        marginLeft: 10
     },
     selectItemType: {
         fontSize: 14,
@@ -75,5 +81,16 @@ const styles = StyleSheet.create({
     typeContainer: {
         flexDirection: 'row',
         marginTop: 6,
+        marginHorizontal: 15,
+        justifyContent: 'space-between'
+    },
+    subContainer: {
+        borderColor:'#222',
+        borderWidth:1,
+        borderRadius:5,
+        flexDirection: 'column',
+        margin: 6,
+        padding:6,
+        width:'50%'
     }
 })
