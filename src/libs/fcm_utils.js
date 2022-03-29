ca95efc273414301ea45a97d158869a933db5f40
Snow <neoprince1992@gmail.com>
2022-03-29 20:08:46 +0900

    disable fcm_utils

--- src/libs/fcm_utils.js
+++ src/libs/fcm_utils.js
@@ -1,43 +1,43 @@
-import firebase from 'react-native-firebase';
+// import firebase from 'react-native-firebase';
 
-export const ReturnFcmToken = async () => {
-    try {
-        const fcmToken = await firebase.messaging().getToken();
-        return fcmToken;
-    } catch (error) {
-        console.warn('ReturnFcmToken', error);
-        return null;
-    }
-};
+// export const ReturnFcmToken = async () => {
+//     try {
+//         const fcmToken = await firebase.messaging().getToken();
+//         return fcmToken;
+//     } catch (error) {
+//         console.warn('ReturnFcmToken', error);
+//         return null;
+//     }
+// };
 
-export const RequestPermission = async () => {
-    try {
-        await firebase.messaging().requestPermission();
-        return true;
-    } catch (error) {
-        console.warn('RequestPermission', error);
-        return false;
-    }
-};
+// export const RequestPermission = async () => {
+//     try {
+//         await firebase.messaging().requestPermission();
+//         return true;
+//     } catch (error) {
+//         console.warn('RequestPermission', error);
+//         return false;
+//     }
+// };
 
-export const CheckPermissionAndReturnFcmToken = async () => {
-    try {
-        const enabled = await firebase.messaging().hasPermission();
-        if (enabled) {
-            const fcmToken = await ReturnFcmToken();
-            return fcmToken;
-        } else {
-            try {
-                await RequestPermission();
-                const fcmToken = await ReturnFcmToken();
-                return fcmToken;
-            } catch (e) {
-                console.warn('CheckPermissionAndReturnFcmToken', error);
-                return null;
-            }
-        }
-    } catch (error) {
-        console.warn('CheckPermissionAndReturnFcmToken', error);
-        return null;
-    }
-};
+// export const CheckPermissionAndReturnFcmToken = async () => {
+//     try {
+//         const enabled = await firebase.messaging().hasPermission();
+//         if (enabled) {
+//             const fcmToken = await ReturnFcmToken();
+//             return fcmToken;
+//         } else {
+//             try {
+//                 await RequestPermission();
+//                 const fcmToken = await ReturnFcmToken();
+//                 return fcmToken;
+//             } catch (e) {
+//                 console.warn('CheckPermissionAndReturnFcmToken', error);
+//                 return null;
+//             }
+//         }
+//     } catch (error) {
+//         console.warn('CheckPermissionAndReturnFcmToken', error);
+//         return null;
+//     }
+// };
